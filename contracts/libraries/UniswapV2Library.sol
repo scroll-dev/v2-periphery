9ad76c79a3ecaf98450cd25f809f2c4236dda7c9
Scroll Dev <dev@scroll.io>
2022-07-06 12:41:30 +0800

    Update UniswapV2Library.sol

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'eaa641206730108a5d03240c05597d08a25dff704ff8d9ed22f55c0229a29695' // init code hash
             ))));
     }
 
